9a6d31ed4f2a2b52a39f08fd02db83160022fd97
victorgargor <victor.gargor@educa.jcyl.es>
2024-09-26 18:53:21 +0200

    Actualización Index y Css todo validado 26/09/2024

---
+++ indexProyectoDWES.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webroot/css/index.css" type="text/css">
+    <title>Víctor García Gordón</title>
+</head>
+<body>
+    <header>      
+        <h1 id="inicio">Desarrollo Web en Entorno Servidor</h1>
+    </header>
+    <main>
+        <nav>
+            <ul>
+                <li><a target="blank" href="doc/EstudioTema1.pdf">TEMA 1 DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>   
+            </ul>
+            <ul>
+                <li>TEMA 2 INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</li>
+                <li><a target="blank" href="doc/EstudioTema2.pdf">EstudioTema2</a></li>            
+            </ul> 
+            <ul>
+                <li><a href="/202DWESProyectoTema3/indexProyectoTema3.php">TEMA 3 CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>        
+            </ul>  
+        </nav>       
+    </main>
+    <footer>
+        <div>
+            <a href="/index.html">Víctor García Gordón</a>
+            <a target="blank" href="doc/curriculum.pdf"><img src="doc/curriculum.jpg" alt="curriculum"></a>
+            <a target="blank" href="https://github.com/victorgargor/202DWESProyectoDWES"><img src="doc/github.png" alt="github"></a>
+        </div>
+    </footer>
+</body>
+</html>

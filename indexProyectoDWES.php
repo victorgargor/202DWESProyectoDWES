9d2e1386b5c4a4a25554cbd635dbd66da5d27194
victorgargor <victor.gargor@educa.jcyl.es>
2024-10-25 13:56:14 +0200

    Clase 25/10/2024

--- indexProyectoDWES.php
+++ indexProyectoDWES.php
@@ -12,15 +12,45 @@
     </header>
     <main>
         <nav>
+            <h2>TEMA 1 DESARROLLO WEB EN ENTORNO SERVIDOR</h2>
             <ul>
-                <li><a target="blank" href="doc/EstudioTema1.pdf">TEMA 1 DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>   
+                <li><a target="blank" href="doc/EstudioTema1.pdf">Desarrollo de aplicaciones web (estudio teórico)</a></li>
+                <li><a target="blank" href="doc/EstudioTema1.pdf">Prestashop (estudio teórico)</a></li>
             </ul>
+            <h2>TEMA 2 INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</h2>
             <ul>
-                <li><a target="blank" href="doc/EstudioTema2.pdf">TEMA 2 INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>            
+                <li><a target="blank" href="doc/EstudioTema2.pdf">Entorno de desarrollo AMP</a></li> 
+                <li><a target="" href="">Funciones y Clases PHP</a></li>
+                <li><a target="" href="">Entorno de desarrollo Prestashop</a></li>
             </ul> 
+            <h2>TEMA 3 CARACTERÍSTICAS DEL LENGUAJE PHP</h2>
             <ul>
-                <li><a href="/202DWESProyectoTema3/indexProyectoTema3.php">TEMA 3 CARACTERÍSTICAS DEL LENGUAJE PHP</a></li>        
+                <li><a href="/202DWESProyectoTema3/indexProyectoTema3.php">Ejercicios Tema 3</a></li>        
             </ul>  
+            <h2>TEMA 4 TÉCNICAS DE ACCESO A DATOS EN PHP</h2>
+            <ul>
+                <li><a href="">Ejercicios Tema 4</a></li>        
+            </ul>
+            <h2>TEMA 5 DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</h2>
+            <ul>
+                <li><a href=""></a></li>        
+            </ul> 
+            <h2>TEMA 6 PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</h2>
+            <ul>
+                <li><a href=""></a></li>        
+            </ul> 
+            <h2>TEMA 7 PROGRAMACIÓN DE SERVICIOS WEB </h2>
+            <ul>
+                <li><a href=""></a></li>        
+            </ul> 
+            <h2>TEMA 8 DESARROLLO DE APLICACIONES WEB HÍBRIDAS</h2>
+            <ul>
+                <li><a href=""></a></li>        
+            </ul> 
+            <h2>TEMA 9 DESARROLLO DE APLICACIONES WEB HÍBRIDAS</h2>
+            <ul>
+                <li><a href=""></a></li>        
+            </ul> 
         </nav>       
     </main>
     <footer>
